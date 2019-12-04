6072430267f92d8598cdcf9b1b5ecefde2ef8bcc
杨水强 <yangshuiqiang@chuangjiangx.com>
2019-12-04 17:21:57 +0800

    feat:添加语音播报

--- app/src/main/java/com/chuangjiangx/kitxandroid/speak/SpeakActivity.kt
+++ app/src/main/java/com/chuangjiangx/kitxandroid/speak/SpeakActivity.kt
@@ -17,8 +17,8 @@ class SpeakActivity : BaseActivity() {
         text.text="1.Application中初始化KitX.init()\n" +
                 "2.Application中初始化SpeakManager.init()\n" +
                 "3.Manifest中添加添加权限ACCESS_NETWORK_STATE 和NETWORK\n" +
-                "4.Manifest中添加<uses-library android:name=\"org.apache.http.legacy\" android:required=\"false\" />\n" +
-                "4.使用SpeakManager.speak()播报"
+                "4.配置kotlin环境\n" +
+                "5.使用SpeakManager.speak()播报"
         SpeakManager.speak("123")
     }
 }
